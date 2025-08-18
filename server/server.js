9a4d665a36971465adf56ad7986e6c71a2a8ed6f
Manojkumar0308 <97066685+Manojkumar0308@users.noreply.github.com>
2025-08-19 04:33:22 +0530

    test

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ app.use(helmet());
 app.use(cors({
   origin: [
     'https://movie-app-eight-lovat.vercel.app',
-    // 'http://localhost:3000',
-    // 'http://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:5173',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
