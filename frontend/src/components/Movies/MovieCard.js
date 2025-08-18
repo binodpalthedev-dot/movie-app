17e3925914a63f586ead472826a5384bb8b5fff3
Manojkumar0308 <97066685+Manojkumar0308@users.noreply.github.com>
2025-08-19 02:26:18 +0530

    ddd

--- frontend/src/components/Movies/MovieCard.js
+++ frontend/src/components/Movies/MovieCard.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const MovieCard = ({ movie }) => { sss
+const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
   const baseURL = "https://movie-app-mbdk.onrender.com";
 
