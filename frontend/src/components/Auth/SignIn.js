8505638986fc7aafcd3d3ab903df3ad1513ff598
Manojkumar0308 <97066685+Manojkumar0308@users.noreply.github.com>
2025-08-20 01:38:35 +0530

    test

--- frontend/src/components/Auth/SignIn.js
+++ frontend/src/components/Auth/SignIn.js
@@ -145,7 +145,7 @@ const SignIn = () => {
       <div className="signin-container">
         <div className="card signin-card">
           <div className="card-body p-5">
-            <h2 className="signin-title text-center">Sign In</h2>
+            <h2 className="signin-title text-center w-100">Sign In</h2>
 
             {errorText && (
               <div className="alert alert-danger">{errorText}</div>
