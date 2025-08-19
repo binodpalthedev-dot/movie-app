3b7e0f2cf5bf572a6ddd1690b2d050cd8585f1a2
Manojkumar0308 <97066685+Manojkumar0308@users.noreply.github.com>
2025-08-19 21:50:33 +0530

    yes

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -19,7 +19,7 @@ const setTokenCookie = (res, token, remember = false) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: truw,
+    secure: true,
     sameSite: isProduction ? 'None' : 'Lax',
     maxAge,
     path: '/'
